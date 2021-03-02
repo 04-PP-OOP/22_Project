cfec3f1c4d24f0d87fe622517f5e973057ff45b0
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-02 07:55:12 +0100

    Add files via upload

---
+++ 08_Project_2_AskACatHerAge/com/cc/java/SuperCat.java
@@ -0,0 +1,59 @@
+package com.cc.java;
+
+public class SuperCat extends Pet{
+
+	protected String name;
+	
+	/**
+	 * @param name
+	 * @param age
+	 */
+	public SuperCat(String name) {
+		this.name = name;
+	}
+
+
+	// Frag irgendeine Katze --> Namen
+	public void askACatItsName() {	
+		System.out.println(Controller.getSomeRandomFelidae().getName(this));
+	}
+	
+	
+	// Frag irgendeine Katze --> Alter
+	// (Cat) felidae | (Tomcat) felidae :: Downcasting
+	public void askACatHerAge() {
+		SuperCat felidae = Controller.getSomeRandomFelidae();
+		if (felidae instanceof Cat) {
+			System.out.println(((Cat) felidae).tellYourAge(this));	
+		} else {
+			System.out.println(((Tomcat) felidae).tellYourAge(this));
+		}
+	}
+	
+
+	/**
+	 * @return the name
+	 */
+	// Frag irgendeine Katze --> Name
+	// (Dog) pet | (SuperCat) pet :: Downcasting
+	public String getName(Pet pet) {
+		
+		if (pet instanceof Dog) 
+		{	
+			return "Sorry " + ((Dog) pet).getName() + ", you're not a cat!";
+		}
+		else {
+			if (((SuperCat) pet).name != this.name) {
+				return "O.K. " + ((SuperCat) pet).name + ", my name is: " + ((SuperCat) pet).name + ".";
+			} else {
+				return "Yes, sometimes you don't know who you are, " + ((SuperCat) pet).name + ".";
+			}
+		}
+	}
+	
+	@Override
+	public void petSounds() {
+		System.out.println(this.name + ": Meeeoow!");	
+	}
+
+}
